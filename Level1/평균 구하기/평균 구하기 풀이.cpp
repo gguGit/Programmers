551ee9f415fb2727b3bdfe0c113495412ee5a8ff
MinJu -Gu <60462331+gguGit@users.noreply.github.com>
2022-05-10 23:56:03 +0900

    Create 평균 구하기 풀이.cpp
    <C++> Programmers 코딩테스트 연습 - 평균 구하기

---
+++ Level1/평균 구하기/평균 구하기 풀이.cpp
@@ -0,0 +1,18 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+double solution(vector<int> arr) 
+{
+    double answer = 0.0;
+    
+    for(auto a:arr)
+    {
+        answer+=a;
+    }
+    
+    answer/=arr.size();
+    
+    return answer;
+}
