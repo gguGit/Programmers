5713145231efa05e2b6aecc2a43e760a6d5129f8
MinJu -Gu <60462331+gguGit@users.noreply.github.com>
2022-05-13 23:51:20 +0900

    Create 문자열 다루기 기본 풀이.cpp
    <C++> Programmers 코딩테스트 연습 - 문자열 다루기 기본 풀이

---
+++ Level1/문자열 다루기 기본/문자열 다루기 기본 풀이.cpp
@@ -0,0 +1,24 @@
+#include <string>
+#include <vector>
+#include <cctype>
+
+using namespace std;
+
+bool solution(string s) 
+{
+    bool answer = true;
+    
+    if(s.length() != 4 && 
+      s.length() != 6)
+    {
+        answer=false;
+    }
+    
+    for(int i=0;i<s.length();i++)
+    {
+        if(!isdigit(s[i]))
+        answer=false;
+    }
+    
+    return answer;
+}
